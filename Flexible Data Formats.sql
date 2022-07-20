3ba5167e4acfe3ab7bb65b6a964abe7fdd335825
ArtourKowalski <108827649+ArtourKowalski@users.noreply.github.com>
2022-07-20 12:42:07 +0200

    Update Flexible Data Formats.sql

--- Flexible Data Formats.sql
+++ Flexible Data Formats.sql
@@ -3,13 +3,13 @@ SELECT
   event_time,
   user_id,
   platform,
-  (
+  MAX(
     CASE
       WHEN parameter_name = 'item_id' THEN CAST(parameter_value AS INT)
       ELSE NULL
     END
   ) AS item_id,
-  (
+  MAX(
     CASE
       WHEN parameter_name = 'referrer' THEN parameter_value
       ELSE NULL
@@ -19,5 +19,9 @@ FROM
   dsv1069.events
 WHERE
   event_name = 'view_item'
-ORDER BY
-  event_id
+GROUP BY
+  event_id,
+  event_time,
+  user_id,
+  platform
+
